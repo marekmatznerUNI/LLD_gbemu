30ad8bf6fef82a04fd034e0f7fdae080b8eb3116
marekmatznerUNI <marek.matzner@rwth-aachen.de>
2023-10-22 16:21:02 +0000

    added comment as a test

---
+++ part1/build/lib/CMakeFiles/emu.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/com.docker.devenvironments.code/part1/lib/bus.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/bus.c.o"
+  "/com.docker.devenvironments.code/part1/lib/cart.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/cart.c.o"
+  "/com.docker.devenvironments.code/part1/lib/cpu.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/cpu.c.o"
+  "/com.docker.devenvironments.code/part1/lib/emu.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/emu.c.o"
+  "/com.docker.devenvironments.code/part1/lib/ppu.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/ppu.c.o"
+  "/com.docker.devenvironments.code/part1/lib/timer.c" "/com.docker.devenvironments.code/part1/build/lib/CMakeFiles/emu.dir/timer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
